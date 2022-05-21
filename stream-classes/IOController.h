65d0ae4f0302bdd617064b5d9e41323e7ccb3fad
Alexander Vladovsky <alexvladovsky@ukr.net>
2022-05-21 19:29:34 +0300

    Add crud operations

--- stream-classes/IOController.h
+++ stream-classes/IOController.h
@@ -23,16 +23,28 @@ public:
       fout.close();
    }
 
-   List<string>* get_all(const string& filename) const
+   static void rewrite(const string& data, const string& filename)
+   {
+      fstream fout(filename, ios::out);
+
+      if (fout.is_open())
+      {
+         fout << data;
+      }
+
+      fout.close();
+   }
+
+   static List<string>* retrieve(const string& filename)
    {
       const auto lines = new LinkedList<string>();
 
-      fstream fin(filename, ios::app);
+      fstream fin(filename);
 
       if (fin.is_open())
       {
          string line;
-         while (getline(fin, line))
+         while (fin >> line)
          {
             lines->add(line);
          }
@@ -41,5 +53,36 @@ public:
       fin.close();
 
       return lines;
+   }
+
+   static void remove(const int index, const string& filename)
+   {
+      const auto lines = retrieve(filename);
+
+      lines->remove(index);
+
+      rewrite(append(lines), filename);
+   }
+
+   static void update(const int index, const employee* object, const string& filename)
+   {
+      const auto lines = retrieve(filename);
+
+      lines->edit(index, object->to_string());
+
+      rewrite(append(lines), filename);
+   }
+
+private:
+
+   static string append(List<string>* lines)
+   {
+      string new_file;
+      for (int i = 0; i < lines->getSize(); i++)
+      {
+         new_file += lines->get(i) + "\n";
+      }
+
+      return new_file;
    }
 };
