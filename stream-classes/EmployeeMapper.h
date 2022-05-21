b5ebaa993a24c6a0d8c0cbd8d9470fb81afde7cb
Alexander Vladovsky <alexvladovsky@ukr.net>
2022-05-21 19:30:12 +0300

    Add file data mapping

--- stream-classes/EmployeeMapper.h
+++ stream-classes/EmployeeMapper.h
@@ -13,7 +13,7 @@ public:
 
    employee_mapper()
    {
-      this->database = io_controller();
+      this->connection_ = io_controller();
    }
 
    static employee* create()
@@ -34,13 +34,55 @@ public:
       return object;
    }
 
-   List<employee*>* map(string path = "db.txt")
+   List<employee*>* map_all(const string& path = "db.txt") const
    {
-      // from file...
+      const auto data = io_controller::retrieve(path);
+      const auto result = new LinkedList<employee*>();
+
+      while (data->getSize() != 0)
+      {
+         result->add(map(split(data->get(0), ":")));
+         data->remove(0);
+      }
+
+      return result;
    }
+
+
 
 private:
 
-   io_controller database;
+   io_controller connection_;
+
+   static string find_one(const string& str, const string& delimiter)
+   {
+      return str.substr(0, str.find(delimiter));
+   }
+
+   static List<string>* split(string str, const string& delimiter)
+   {
+      const auto elements = new LinkedList<string>();
+
+      while (str.find(delimiter) != -1)
+      {
+         string element = find_one(str, delimiter);
+         elements->add(element);
+
+         str = str.substr(element.length() + 1);
+      }
+
+      return elements;
+   }
+
+   employee* map(List<string>* data) const
+   {
+      const auto object = new employee();
+
+      object->set_name(data->get(0));
+      object->set_age(stoi(data->get(1)));
+      object->set_experience(stoi(data->get(2)));
+
+      return object;
+   }
 
 };
